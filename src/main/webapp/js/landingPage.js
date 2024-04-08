f774f5fb09f33ddf9c29386aae86cd7d7890a8c2
Kevin Hu <kevinhu229@gmail.com>
2024-04-07 23:08:16 -0700

    initial commit

---
+++ src/main/webapp/js/landingPage.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const diningOptions = [
+        { name: "Everybody's Kitchen", activityLevel: '63%', waitTime: '20 min.', logo: 'assets/everybodys_kitchen_logo.png' },
+        { name: "Parkside", activityLevel: '91%', waitTime: '30 min.', logo: 'assets/parkside_logo.png' },
+        { name: "USC Village Dining Hall", activityLevel: '27%', waitTime: '10 min.', logo: 'assets/usc_village_logo.png'}
+    ];
+
+    const diningOptionsContainer = document.getElementById('dining-options');
+
+    diningOptions.forEach(option => {
+        const optionElement = document.createElement('div');
+        optionElement.classList.add('option');
+
+        optionElement.innerHTML = `
+            <div class="logo-container">
+                <img class="dining-hall-logo" src="${option.logo}" alt="Dining Hall Logo" />
+            </div>
+            <div class="name">${option.name}</div>
+            <div class="status-bar">
+                <img src="assets/users.png" alt="Timer" class="icon" />
+                <div class="bar-background">
+                    <div class="activity-bar" style="width: ${option.activityLevel};"></div>
+                </div>
+                <span class="activity-level">${option.activityLevel}</span>                
+            </div>
+            <div class="wait-time">
+                <span class="wait-time">
+                    <img src="assets/clock.png" alt="Timer" class="icon" />
+                    <span class="wait-time-text">${option.waitTime}</span>   
+                </span>
+            </div>
+        `;
+
+        diningOptionsContainer.appendChild(optionElement);
+    });
+});
