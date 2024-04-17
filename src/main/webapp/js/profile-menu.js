b1cf768a69ced461c397c411efe52d172ae60c29
Jad Barmada <barmada@usc.edu>
2024-04-16 18:05:01 -0700

    started working on location toggle

--- src/main/webapp/js/profile-menu.js
+++ src/main/webapp/js/profile-menu.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     arrow.addEventListener("click", function() {
         //menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
         menu.style.display = "flex";
+        
     });
 
     // Close the dropdown when clicking outside of it
@@ -18,4 +19,21 @@ document.addEventListener("DOMContentLoaded", function() {
 function logout(){
     // Print "logout worked!" text
     console.log("logout worked!");
+}
+
+function locationToggle(){
+    // Get the elements that need to be modified
+    var optionWrapper = document.querySelector(".OptionWrapper");
+    var titleArea = document.querySelector(".TitleArea");
+    var locationToggleBtn = document.querySelector(".LocationToggle");
+
+    // Hide the current contents of the menu
+    optionWrapper.style.display = "none";
+
+    // Update the title of the menu to "Location"
+    var title = titleArea.querySelector("#expanded-username");
+    title.textContent = "Location";
+
+    // Show the location toggle button
+    locationToggleBtn.style.display = "block";
 }
